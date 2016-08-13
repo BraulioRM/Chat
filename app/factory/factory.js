0b41c3b84b7991e46f2febd3f3dd14e971779545
braulio <ramirez.braulioa@gmail.com>
2016-08-13 18:12:13 -0500

    my first commit

---
+++ app/factory/factory.js
@@ -0,0 +1,16 @@
+(function(){
+
+'use strict'
+
+angular
+    .module('chat')
+    .factory('chatData',chatData);
+
+
+function chatData($firebaseArray){
+    var link = new Firebase('https://chatnegrabatch10.firebaseio.com/message')
+    return $firebaseArray(link)
+}
+
+
+})();
